c720c53c17f618601e7b249e92a7f8874fde2384
Bob Langley <bob.langley@particular.net>
2017-09-02 23:15:54 -0700

    Let AuditHostInformationBehavior handle ProcessingEndpoint header

--- src/RetrySuccessNotification/RetrySuccessNotificationDispatchConnector.cs
+++ src/RetrySuccessNotification/RetrySuccessNotificationDispatchConnector.cs
@@ -7,20 +7,11 @@ namespace NServiceBus
 
     class RetrySuccessNotificationDispatchConnector : StageConnector<IAuditContext, IRoutingContext>
     {
-        public RetrySuccessNotificationDispatchConnector(string endpointName)
-        {
-            this.endpointName = endpointName;
-        }
-
         public override Task Invoke(IAuditContext context, Func<IRoutingContext, Task> stage)
         {
-            context.Message.Headers.Add(Headers.ProcessingEndpoint, endpointName);
-
             var dispatchContext = this.CreateRoutingContext(context.Message, new UnicastRoutingStrategy(context.AuditAddress), context);
 
             return stage(dispatchContext);
         }
-
-        string endpointName;
     }
 }
