21ce9631122893b3c8c212e5fe13ab42b602aa36
Bob Langley <bob.langley@particular.net>
2017-09-07 21:09:19 -0700

    Move test to root namespace

--- src/Tests/Config/RetrySuccessNotificationConfigTests.cs
+++ src/Tests/Config/RetrySuccessNotificationConfigTests.cs
@@ -1,72 +1,69 @@
-﻿namespace NServiceBus.Recoverability.RetrySucessNotification.ComponentTests
+﻿using System.Linq;
+using NServiceBus;
+using NServiceBus.Configuration.AdvanceExtensibility;
+using NServiceBus.Features;
+using NUnit.Framework;
+
+[TestFixture]
+public class RetrySuccessNotificationConfigTests
 {
-    using System.Linq;
-    using NServiceBus;
-    using Configuration.AdvanceExtensibility;
-    using Features;
-    using NUnit.Framework;
-
-    [TestFixture]
-    public class RetrySuccessNotificationConfigTests
+    [Test]
+    public void Notification_Address_Can_Be_Set()
     {
-        [Test]
-        public void Notification_Address_Can_Be_Set()
+        var endpointConfiguration = new EndpointConfiguration("test");
+
+        var config = endpointConfiguration.RetrySuccessNotifications();
+
+        const string testAddress = "Test";
+
+        config.SendRetrySuccessNotificationsTo(testAddress);
+
+        var settings = endpointConfiguration.GetSettings();
+
+        var settingRetrieved = settings.TryGet(RetrySuccessNotification.AddressKey, out string notificationAddress);
+
+        Assert.IsTrue(settingRetrieved, "Setting was not set");
+        Assert.AreEqual(testAddress, notificationAddress, "Incorrect Notification Address value");
+    }
+
+    [Test]
+    public void Trigger_Headers_Can_Be_Added()
+    {
+        var endpointConfiguration = new EndpointConfiguration("test");
+
+        var config = endpointConfiguration.RetrySuccessNotifications();
+
+        const string testHeader = "Test";
+
+        config.AddRetrySuccessNotificationTriggerHeaders(testHeader);
+
+        var settings = endpointConfiguration.GetSettings();
+
+        var settingRetrieved = settings.TryGet(RetrySuccessNotification.TriggerHeadersKey, out string[] triggerHeaders);
+
+        var expectedHeaders = RetrySuccessNotification.DefaultTriggerHeaders.Union(new[]
         {
-            var endpointConfiguration = new EndpointConfiguration("test");
+            testHeader
+        });
 
-            var config = endpointConfiguration.RetrySuccessNotifications();
+        Assert.IsTrue(settingRetrieved, "Setting was not set");
+        Assert.That(triggerHeaders, Is.EquivalentTo(expectedHeaders), "Headers are missing");
+    }
 
-            const string testAddress = "Test";
+    [Test]
+    public void Copy_Message_Body_Setting_Can_Be_Set()
+    {
+        var endpointConfiguration = new EndpointConfiguration("test");
 
-            config.SendRetrySuccessNotificationsTo(testAddress);
+        var config = endpointConfiguration.RetrySuccessNotifications();
 
-            var settings = endpointConfiguration.GetSettings();
+        config.CopyMessageBodyInNotification = true;
 
-            var settingRetrieved = settings.TryGet(RetrySuccessNotification.AddressKey, out string notificationAddress);
+        var settings = endpointConfiguration.GetSettings();
 
-            Assert.IsTrue(settingRetrieved, "Setting was not set");
-            Assert.AreEqual(testAddress, notificationAddress, "Incorrect Notification Address value");
-        }
+        var settingRetrieved = settings.TryGet(RetrySuccessNotification.CopyBody, out bool copyBodySetting);
 
-        [Test]
-        public void Trigger_Headers_Can_Be_Added()
-        {
-            var endpointConfiguration = new EndpointConfiguration("test");
-
-            var config = endpointConfiguration.RetrySuccessNotifications();
-
-            const string testHeader = "Test";
-
-            config.AddRetrySuccessNotificationTriggerHeaders(testHeader);
-
-            var settings = endpointConfiguration.GetSettings();
-
-            var settingRetrieved = settings.TryGet(RetrySuccessNotification.TriggerHeadersKey, out string[] triggerHeaders);
-
-            var expectedHeaders = RetrySuccessNotification.DefaultTriggerHeaders.Union(new[]
-            {
-                testHeader
-            });
-
-            Assert.IsTrue(settingRetrieved, "Setting was not set");
-            Assert.That(triggerHeaders, Is.EquivalentTo(expectedHeaders), "Headers are missing");
-        }
-
-        [Test]
-        public void Copy_Message_Body_Setting_Can_Be_Set()
-        {
-            var endpointConfiguration = new EndpointConfiguration("test");
-
-            var config = endpointConfiguration.RetrySuccessNotifications();
-
-            config.CopyMessageBodyInNotification = true;
-
-            var settings = endpointConfiguration.GetSettings();
-
-            var settingRetrieved = settings.TryGet(RetrySuccessNotification.CopyBody, out bool copyBodySetting);
-
-            Assert.IsTrue(settingRetrieved, "Setting was not set");
-            Assert.IsTrue(copyBodySetting, "Incorrect Copy Body Setting value");
-        }
+        Assert.IsTrue(settingRetrieved, "Setting was not set");
+        Assert.IsTrue(copyBodySetting, "Incorrect Copy Body Setting value");
     }
 }
